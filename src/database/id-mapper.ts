af52d0f1a67db0c79f481775d2e13dfac5d60f89
Edouard Demotes <e.demotes@gmail.com>
2023-10-23 23:40:44 -0400

    test: add tests for IdMapper

--- src/database/id-mapper.ts
+++ src/database/id-mapper.ts
@@ -2,92 +2,90 @@ import {Knex} from "knex";
 
 type PrimaryKeyType = 'integer' | 'string';
 
+export type IdMap = {
+    table: string;
+    sync_id: string;
+    local_id: string;
+    created_at: Date;
+}
 
 export class IdMapper {
 
-    protected readonly table = 'directus_sync_id_map';
+    protected readonly tableName = 'directus_sync_id_map';
 
-    protected directusTablePrefix = 'directus_';
+    protected readonly directusTablePrefix = 'directus_';
 
     protected tablePrimaryKeys: Record<string, PrimaryKeyType> | undefined;
 
     constructor(protected readonly database: Knex) {
 
     }
-     async init() {
-        if (!await this.database.schema.hasTable(this.table)) {
-            await this.database.schema.createTable(this.table, (table) => {
+
+    /**
+     * Init the id mapper by creating the table if it doesn't exist
+     */
+    async init() {
+        if (!await this.database.schema.hasTable(this.tableName)) {
+            await this.database.schema.createTable(this.tableName, (table) => {
                 table.string('table').notNullable();
                 table.string('sync_id').notNullable();
                 table.string('local_id').notNullable();
+                table.timestamp('created_at').defaultTo(this.database.fn.now());
                 table.primary(['table', 'sync_id']);
-                table.index(['table', 'local_id']);
+                table.unique(['table', 'local_id']);
+                table.index(['created_at']);
             });
         }
      }
 
-
     /**
-     * Returns all tables that start with the directus prefix
+     * Get the table name
      */
-    protected async getDirectusTables(): Promise<string[]> {
-        const tables = await this.database.raw('SHOW TABLES');
-        return tables[0].filter((table: string) => table.startsWith(this.directusTablePrefix));
-    }
-
-    /**
-     * For each table, denotes if the primary key is an auto-incrementing integer or a string (UUID)
-     */
-    protected async getTablePrimaryKeysTypes(): Promise<Record<string, PrimaryKeyType>> {
-        const tables = await this.getDirectusTables();
-        const tablePrimaryKeys: Record<string, PrimaryKeyType> = {};
-        for (const table of tables) {
-            const columns = await this.database.raw(`SHOW COLUMNS FROM ${table}`);
-            const primaryKey = columns[0].find((column: any) => column.Key === 'PRI');
-            if (primaryKey) {
-                tablePrimaryKeys[table] = primaryKey.Type.startsWith('int') ? 'integer' : 'string';
-            }
-        }
-        return tablePrimaryKeys;
-    }
-
-    /**
-     * Defines the primary keys for each table and caches the result.
-     * Returns the primary key for the given table.
-     */
-    protected async getTablePrimaryKeyType(table: string): Promise<PrimaryKeyType> {
-        if (!this.tablePrimaryKeys) {
-            this.tablePrimaryKeys = await this.getTablePrimaryKeysTypes();
-        }
-        // If the table doesn't have a primary key, we can't sync it, throw an error
-        const primaryKeyType = this.tablePrimaryKeys[table];
-        if (!primaryKeyType) {
-            throw new Error(`Table ${table} is not a Directus table or does not have a primary key`);
-        }
-        return primaryKeyType;
+    getTableName(): string {
+        return this.tableName;
     }
 
     /**
      * Returns the local key for the given table and sync id
      */
-    async getLocalId(table: string, syncId: string): Promise<number | string> {
-        const primaryKeyType = await this.getTablePrimaryKeyType(table);
-        const result = await this.database(this.table).where({ table, sync_id: syncId }).first();
+    async getLocalId(table: string, syncId: string): Promise<string> {
+        const result: IdMap = await this.database(this.tableName).where({ table, sync_id: syncId }).first();
         if (!result) {
             throw new Error(`No local id found for table ${table} and sync id ${syncId}`);
         }
         // Map the local id to the correct type
-        return primaryKeyType === 'integer' ? parseInt(result.local_id) : result.local_id.toString();
+        return result.local_id;
     }
 
     /**
      * Returns the sync id for the given table and local id
      */
-    async getSyncId(table: string, localId: number | string): Promise<string> {
-        const result = await this.database(this.table).where({ table, local_id: localId }).first();
+    async getSyncId(table: string, localId: string): Promise<string> {
+        const result: IdMap = await this.database(this.tableName).where({ table, local_id: localId }).first();
         if (!result) {
             throw new Error(`No sync id found for table ${table} and local id ${localId}`);
         }
         return result.sync_id;
+    }
+
+    /**
+     * Adds a new entry to the id map
+     */
+    async add(table: string, syncId: string, localId: number | string): Promise<void> {
+        await this.database(this.tableName).insert({ table, sync_id: syncId, local_id: localId });
+    }
+
+    /**
+     * Removes an entry from the id map using the sync id
+     */
+    async removeBySyncId(table: string, syncId: string): Promise<void> {
+        await this.database(this.tableName).where({ table, sync_id: syncId }).delete();
+    }
+
+    /**
+     * Removes an entry from the id map using the local id
+     */
+    async removeByLocalId(table: string, localId: number | string): Promise<void> {
+        await this.database(this.tableName).where({ table, local_id: localId }).delete();
     }
 }
