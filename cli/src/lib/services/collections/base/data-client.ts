df8219ad82c16b0499c3dc7ab54fba02fa175a9e
Edouard Demotes <e.demotes@gmail.com>
2023-11-13 11:30:59 -0500

    feat: exclude current user role from dump

--- cli/src/lib/services/collections/base/data-client.ts
+++ cli/src/lib/services/collections/base/data-client.ts
@@ -1,78 +1,79 @@
-import { Query, RestCommand } from '@directus/sdk';
-import { DirectusBaseType, DirectusId, WithoutIdAndSyncId } from './interfaces';
-import { MigrationClient } from '../../migration-client';
+import {Query, RestCommand} from '@directus/sdk';
+import {DirectusBaseType, DirectusId, WithoutIdAndSyncId} from './interfaces';
+import {MigrationClient} from '../../migration-client';
 
 /**
  * This class is responsible for calling CRUD operations on the target collection using the rest API.
  */
 export abstract class DataClient<DirectusType extends DirectusBaseType> {
-  constructor(protected readonly migrationClient: MigrationClient) {}
-
-  /**
-   * Request data from the target collection using the rest API.
-   */
-  async query<T extends object = DirectusType>(
-    query: Query<DirectusType, object>,
-  ): Promise<T[]> {
-    const directus = this.migrationClient.get();
-    const response = await directus.request<T | T[]>(
-      this.getQueryCommand(query),
-    );
-    // Some collections, such as settings, return a single object instead of an array or may return a fake item, without id.
-    if (Array.isArray(response)) {
-      return response;
-    } else {
-      if (!(response as DirectusType).id) {
-        return [];
-      }
-      return [response as T];
+    constructor(protected readonly migrationClient: MigrationClient) {
     }
-  }
 
-  /**
-   * Inserts data into the target collection using the rest API.
-   * Remove the id and the syncId from the item before inserting it.
-   */
-  async create(item: WithoutIdAndSyncId<DirectusType>): Promise<DirectusType> {
-    const directus = this.migrationClient.get();
-    return await directus.request(this.getInsertCommand(item));
-  }
+    /**
+     * Request data from the target collection using the rest API.
+     */
+    async query<T extends object = DirectusType>(
+        query: Query<DirectusType, object>,
+    ): Promise<T[]> {
+        const directus = this.migrationClient.get();
+        const response = await directus.request<T | T[]>(
+            await this.getQueryCommand(query),
+        );
+        // Some collections, such as settings, return a single object instead of an array or may return a fake item, without id.
+        if (Array.isArray(response)) {
+            return response;
+        } else {
+            if (!(response as DirectusType).id) {
+                return [];
+            }
+            return [response as T];
+        }
+    }
 
-  /**
-   * Updates data in the target collection using the rest API.
-   * The diff item already excludes the id and the syncId.
-   */
-  async update(
-    itemId: DirectusId,
-    diffItem: Partial<WithoutIdAndSyncId<DirectusType>>,
-  ): Promise<DirectusType> {
-    const directus = this.migrationClient.get();
-    return await directus.request(this.getUpdateCommand(itemId, diffItem));
-  }
+    /**
+     * Inserts data into the target collection using the rest API.
+     * Remove the id and the syncId from the item before inserting it.
+     */
+    async create(item: WithoutIdAndSyncId<DirectusType>): Promise<DirectusType> {
+        const directus = this.migrationClient.get();
+        return await directus.request(await this.getInsertCommand(item));
+    }
 
-  /**
-   * Deletes data from the target collection using the rest API.
-   * The id is the local id.
-   */
-  async delete(itemId: DirectusId): Promise<DirectusType> {
-    const directus = this.migrationClient.get();
-    return await directus.request(this.getDeleteCommand(itemId));
-  }
+    /**
+     * Updates data in the target collection using the rest API.
+     * The diff item already excludes the id and the syncId.
+     */
+    async update(
+        itemId: DirectusId,
+        diffItem: Partial<WithoutIdAndSyncId<DirectusType>>,
+    ): Promise<DirectusType> {
+        const directus = this.migrationClient.get();
+        return await directus.request(await this.getUpdateCommand(itemId, diffItem));
+    }
 
-  protected abstract getQueryCommand(
-    query: Query<DirectusType, object>,
-  ): RestCommand<DirectusType[], object>;
+    /**
+     * Deletes data from the target collection using the rest API.
+     * The id is the local id.
+     */
+    async delete(itemId: DirectusId): Promise<DirectusType> {
+        const directus = this.migrationClient.get();
+        return await directus.request(await this.getDeleteCommand(itemId));
+    }
 
-  protected abstract getInsertCommand(
-    item: WithoutIdAndSyncId<DirectusType>,
-  ): RestCommand<DirectusType, object>;
+    protected abstract getQueryCommand(
+        query: Query<DirectusType, object>,
+    ): RestCommand<DirectusType[], object> | Promise<RestCommand<DirectusType[], object>>;
 
-  protected abstract getUpdateCommand(
-    itemId: DirectusId,
-    diffItem: Partial<WithoutIdAndSyncId<DirectusType>>,
-  ): RestCommand<DirectusType, object>;
+    protected abstract getInsertCommand(
+        item: WithoutIdAndSyncId<DirectusType>,
+    ): RestCommand<DirectusType, object> | Promise<RestCommand<DirectusType, object>>;
 
-  protected abstract getDeleteCommand(
-    itemId: DirectusId,
-  ): RestCommand<DirectusType, object>;
+    protected abstract getUpdateCommand(
+        itemId: DirectusId,
+        diffItem: Partial<WithoutIdAndSyncId<DirectusType>>,
+    ): RestCommand<DirectusType, object> | Promise<RestCommand<DirectusType, object>>;
+
+    protected abstract getDeleteCommand(
+        itemId: DirectusId,
+    ): RestCommand<DirectusType, object> | Promise<RestCommand<DirectusType, object>>;
 }
--- cli/src/lib/services/migration-client.ts
+++ cli/src/lib/services/migration-client.ts
@@ -1,38 +1,56 @@
 import {
-  authentication,
-  AuthenticationClient,
-  createDirectus,
-  DirectusClient,
-  rest,
-  RestClient,
+    authentication,
+    AuthenticationClient,
+    createDirectus,
+    DirectusClient,
+    readMe,
+    rest,
+    RestClient,
 } from '@directus/sdk';
-import { Inject, Service } from 'typedi';
+import {Inject, Service} from 'typedi';
 import pino from 'pino';
-import { DIRECTUS_CONFIG, LOGGER } from '../constants';
-import type { DirectusConfig } from '../config';
+import {DIRECTUS_CONFIG, LOGGER} from '../constants';
+import type {DirectusConfig} from '../config';
 
 @Service()
 export class MigrationClient {
-  protected readonly client: DirectusClient<any> &
-    RestClient<any> &
-    AuthenticationClient<any>;
 
-  constructor(
-    @Inject(DIRECTUS_CONFIG) protected readonly config: DirectusConfig,
-    @Inject(LOGGER) protected readonly logger: pino.Logger,
-  ) {
-    this.client = this.createClient();
-  }
+    protected userRoleId: string | undefined;
 
-  get() {
-    return this.client;
-  }
+    protected readonly client: DirectusClient<any> &
+        RestClient<any> &
+        AuthenticationClient<any>;
 
-  protected createClient() {
-    const client = createDirectus(this.config.url)
-      .with(rest())
-      .with(authentication());
-    client.setToken(this.config.token);
-    return client;
-  }
+    constructor(
+        @Inject(DIRECTUS_CONFIG) protected readonly config: DirectusConfig,
+        @Inject(LOGGER) protected readonly logger: pino.Logger,
+    ) {
+        this.client = this.createClient();
+    }
+
+    get() {
+        return this.client;
+    }
+
+    /**
+     * This methods return the role of the current user
+     */
+    async getUserRoleId() {
+        if (!this.userRoleId) {
+            const directus = this.get();
+            const {role} = await directus.request(readMe({
+                fields: ['role'],
+            }));
+            this.userRoleId = role;
+        }
+        return this.userRoleId;
+    }
+
+    protected createClient() {
+        const client = createDirectus(this.config.url)
+            .with(rest())
+            .with(authentication());
+        client.setToken(this.config.token);
+        return client;
+    }
 }
