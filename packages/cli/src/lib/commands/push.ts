6dce39795e040eed516cb4045b68b257bdbc97be
Edouard Demotes <e.demotes@gmail.com>
2023-11-18 15:47:42 -0500

    feat: add lerna

---
+++ packages/cli/src/lib/commands/push.ts
@@ -0,0 +1,37 @@
+import { Container } from 'typedi';
+import pino from 'pino';
+import { SnapshotClient } from '../services';
+import { loadCollections } from '../loader';
+import { LOGGER } from '../constants';
+
+export async function runPush() {
+  const logger = Container.get(LOGGER) as pino.Logger;
+
+  // Snapshot
+  logger.info(`---- Push schema ----`);
+  await Container.get(SnapshotClient).push();
+
+  // Collections
+  const collections = loadCollections();
+
+  // Clean up the collections (dangling id maps, etc.)
+  logger.info(`---- Clean up collections ----`);
+  for (const collection of collections) {
+    await collection.cleanUp();
+  }
+
+  // Restore collections until there is nothing to restore
+  let stop = false;
+  let index = 1;
+  while (!stop) {
+    logger.info(`---- Push: iteration ${index} ----`);
+    stop = true;
+    for (const collection of collections) {
+      const retry = await collection.push();
+      if (retry) {
+        stop = false;
+      }
+    }
+    index++;
+  }
+}
