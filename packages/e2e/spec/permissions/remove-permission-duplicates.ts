153fe06ad34d8494b1295c7c6c6898eb9690f57e
Edouard Demotes <e.demotes@gmail.com>
2024-05-02 10:14:19 -0400

    test(e2e): add test if no duplicates

--- packages/e2e/spec/permissions/remove-permission-duplicates.ts
+++ packages/e2e/spec/permissions/remove-permission-duplicates.ts
@@ -87,4 +87,35 @@ export const removePermissionDuplicates = (context: Context) => {
     expect(permissions.some((p) => p.id === delete1.id)).toBeTrue();
     expect(permissions.some((p) => p.id === delete2.id)).toBeFalse();
   });
+
+  it('should not remove permissions if no duplicates', async () => {
+    // Init sync client
+    const sync = await context.getSync('temp/remove-permission-duplicates');
+    const directus = context.getDirectus();
+    const client = directus.get();
+
+    // Create permissions
+    const role = await newRole(client);
+    const update1 = await newPermission(client, role.id, 'panels', 'update');
+    const create1 = await newPermission(client, role.id, 'panels', 'create');
+    const delete1 = await newPermission(client, null, 'panels', 'delete');
+    const read1 = await newPermission(client, null, 'panels', 'read');
+
+    // Dump the data
+    await sync.removePermissionDuplicates('first');
+
+    // Able to read the permissions
+    const permissions = (
+      await client.request<PermissionWithSystem[]>(
+        readPermissions({
+          fields: ['id', 'role', 'collection', 'action'],
+        }),
+      )
+    ).filter((p) => !p.system);
+
+    expect(permissions.some((p) => p.id === update1.id)).toBeTrue();
+    expect(permissions.some((p) => p.id === create1.id)).toBeTrue();
+    expect(permissions.some((p) => p.id === delete1.id)).toBeTrue();
+    expect(permissions.some((p) => p.id === read1.id)).toBeTrue();
+  });
 };
