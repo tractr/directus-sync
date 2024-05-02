3f735949a8997916bef95fc4938f3356371ad42c
Edouard Demotes <e.demotes@gmail.com>
2024-05-02 12:44:33 -0400

    fix: add safe logger in entrypoint

--- packages/cli/src/run.ts
+++ packages/cli/src/run.ts
@@ -2,16 +2,22 @@ import { createProgram, LOGGER } from './lib';
 import pino from 'pino';
 import { Container } from 'typedi';
 
+function getLogger(): pino.Logger | Console {
+  return Container.has(LOGGER) ?
+    Container.get(LOGGER) :
+    console;
+}
+
 export function run() {
   return createProgram()
     .parseAsync(process.argv)
     .catch((error: string | Error) => {
-      const logger: pino.Logger = Container.get(LOGGER);
+      const logger = getLogger();
       logger.error(error);
       process.exit(1);
     })
     .then(() => {
-      const logger: pino.Logger = Container.get(LOGGER);
+      const logger = getLogger();
       logger.info(`✅  Done!`);
       process.exit(0);
     });
