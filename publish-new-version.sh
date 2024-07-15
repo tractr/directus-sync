7858751026925a1151ac0e32ced3bfdeb58f8d02
Edouard Demotes <e.demotes@gmail.com>
2024-07-15 09:24:23 -0400

    ci: add manual publish script

---
+++ publish-new-version.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -e
+
+npm install
+npm run format
+npm run lint
+npm run build
+npm run bootstrap
+npm run test
+
+# Ask confirmation before creating a new version
+read -p "Do you want to create a new version? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+# Create a new version
+npm run lerna version
+
+# Ask confirmation before publishing
+read -p "Do you want to publish the new version? (y/n) " -n 1 -r
+echo
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+
+# Publish the new version
+npm run lerna publish from-package
+
