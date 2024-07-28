2d95c5d380cfb2572c9e31f31103c591f5c3d3d7
Edouard Demotes <e.demotes@gmail.com>
2024-07-27 22:50:42 -0400

    feat(cli): allow table name override

--- packages/cli/src/lib/services/seed/id-mapper-client.ts
+++ packages/cli/src/lib/services/seed/id-mapper-client.ts
@@ -1,8 +1,20 @@
 import { IdMapperClient } from '../collections';
 import { MigrationClient } from '../migration-client';
+import { Cacheable } from 'typescript-cacheable';
 
-export class SeedIdMapperClient extends IdMapperClient {
+const DIRECTUS_COLLECTIONS_PREFIX = 'directus_';
+const CUSTOM_COLLECTIONS_PREFIX = 'cus:';
+
+export class al extends IdMapperClient {
   constructor(migrationClient: MigrationClient, collection: string) {
     super(migrationClient, collection);
+  }
+
+  @Cacheable()
+  protected getStoredTableName() {
+    if (this.table.startsWith(DIRECTUS_COLLECTIONS_PREFIX)) {
+      return this.table.slice(DIRECTUS_COLLECTIONS_PREFIX.length);
+    }
+    return `${CUSTOM_COLLECTIONS_PREFIX}:${this.table}`;
   }
 }
