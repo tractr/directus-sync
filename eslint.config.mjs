f474f5c2596b374ef5d519df8562cffcc1494a9b
Edouard Demotes <e.demotes@gmail.com>
2024-08-15 21:35:59 -0400

    chore: update root deps

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,57 +1,64 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
+export default [
+  {
     ignores: [
-        "**/dist/",
-        "**/node_modules/",
-        "**/directus/",
-        "**/*.js",
-        "**/*.cjs",
-        "**/*.mjs",
+      '**/dist/',
+      '**/node_modules/',
+      '**/directus/',
+      '**/*.js',
+      '**/*.cjs',
+      '**/*.mjs',
     ],
-}, ...compat.extends(
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended-type-checked",
-    "plugin:@typescript-eslint/stylistic-type-checked",
-    "prettier",
-), {
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'prettier',
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      '@typescript-eslint': typescriptEslint,
     },
 
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 5,
-        sourceType: "script",
+      parser: tsParser,
+      ecmaVersion: 5,
+      sourceType: 'script',
 
-        parserOptions: {
-            project: ["./packages/*/tsconfig.eslint.json"],
-        },
+      parserOptions: {
+        project: ['./packages/*/tsconfig.eslint.json'],
+      },
     },
 
     rules: {
-        "@typescript-eslint/no-unsafe-assignment": "off",
-        "@typescript-eslint/no-unsafe-argument": "off",
-        "@typescript-eslint/no-unsafe-return": "off",
-        "@typescript-eslint/no-unsafe-call": "off",
-        "@typescript-eslint/no-misused-promises": "off",
-        "@typescript-eslint/no-unsafe-member-access": "off",
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-argument': 'off',
+      '@typescript-eslint/no-unsafe-return': 'off',
+      '@typescript-eslint/no-unsafe-call': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
 
-        "@typescript-eslint/no-unused-vars": ["error", {
-            ignoreRestSiblings: true,
-        }],
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          ignoreRestSiblings: true,
+        },
+      ],
     },
-}];
+  },
+];
