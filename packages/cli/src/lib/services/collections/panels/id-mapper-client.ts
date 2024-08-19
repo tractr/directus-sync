658ffd5913ccdad8d1e634c44caebfdcc9e82170
Edouard Demotes <e.demotes@gmail.com>
2024-08-19 12:05:22 -0400

    chore(cli): add logger in id-mappers

--- packages/cli/src/lib/services/collections/panels/id-mapper-client.ts
+++ packages/cli/src/lib/services/collections/panels/id-mapper-client.ts
@@ -1,11 +1,21 @@
 import { IdMapperClient } from '../base';
-import { Service } from 'typedi';
+import { Inject, Service } from 'typedi';
 import { PANELS_COLLECTION } from './constants';
 import { MigrationClient } from '../../migration-client';
+import { LOGGER } from '../../../constants';
+import pino from 'pino';
+import { getChildLogger } from '../../../helpers';
 
 @Service()
 export class PanelsIdMapperClient extends IdMapperClient {
-  constructor(migrationClient: MigrationClient) {
-    super(migrationClient, PANELS_COLLECTION);
+  constructor(
+    migrationClient: MigrationClient,
+    @Inject(LOGGER) baseLogger: pino.Logger,
+  ) {
+    super(
+      migrationClient,
+      getChildLogger(baseLogger, PANELS_COLLECTION),
+      PANELS_COLLECTION,
+    );
   }
 }
