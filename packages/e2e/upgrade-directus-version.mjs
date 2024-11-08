db38fbc5a2239818bb438035ee0c472877764918
Edouard Demotes <e.demotes@gmail.com>
2024-11-08 10:22:27 -0500

    chore: update tests snapshots

--- packages/e2e/upgrade-directus-version.mjs
+++ packages/e2e/upgrade-directus-version.mjs
@@ -12,7 +12,7 @@ async function writeJSON(path, data) {
 }
 
 const actual = '11.1.2';
-const next = '11.1.2';
+const next = '11.2.0';
 
 if (actual === next) {
   console.log('Nothing to upgrade');
