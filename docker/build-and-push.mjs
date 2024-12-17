469e786f58e58ee78e5f56480f09e227b4c1fcd5
Edouard Demotes <e.demotes@gmail.com>
2024-12-17 13:02:50 -0500

    chore: bump to Directus 11.3.4

--- docker/build-and-push.mjs
+++ docker/build-and-push.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 
-const latestDirectusVersion = '11.3.3';
+const latestDirectusVersion = '11.3.4';
 const [major, minor, patch] = latestDirectusVersion.split('.');
 const directusVersions = [
   'latest',
