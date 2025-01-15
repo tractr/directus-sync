216034f59fe9ec46f287054267b2922a09987838
Edouard Demotes <e.demotes@gmail.com>
2025-01-15 14:01:47 -0500

    fix: export all enums

--- packages/cli/src/lib/services/snapshot/index.ts
+++ packages/cli/src/lib/services/snapshot/index.ts
@@ -1,2 +1,15 @@
-export type { Snapshot, Type } from './interfaces';
+export type { Snapshot } from './interfaces';
+export {
+  Accountability,
+  Collapse,
+  Group,
+  Display,
+  Template,
+  Width,
+  DataType,
+  ForeignKeyColumn,
+  Type,
+  OneDeselectAction,
+  On,
+} from './interfaces';
 export * from './snapshot-client';
