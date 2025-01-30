ed11dcc01b587715c1652bef461eac145c5711dc
Edouard Demotes <e.demotes@gmail.com>
2025-01-30 00:52:10 -0500

    chore: change meta title and description

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -34,8 +34,8 @@ export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      title={siteConfig.title}
+      description={siteConfig.tagline}
     >
       <HomepageHeader />
       <main>
