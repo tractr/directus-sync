66793ef2037950c078460248708b1801905a37ba
Edouard Demotes <e.demotes@gmail.com>
2025-03-25 13:51:33 -0400

    chore: update snapshots to 11.5.0

--- docker/build-and-push.mjs
+++ docker/build-and-push.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 
-const latestDirectusVersion = '11.4.1';
+const latestDirectusVersion = '11.5.0';
 const [major, minor, patch] = latestDirectusVersion.split('.');
 const directusVersions = [
   'latest',
