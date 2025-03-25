03bb6cc7e33d6b007b3e06f88edb2507cef512ce
Edouard Demotes <e.demotes@gmail.com>
2025-03-25 14:01:04 -0400

    chore: update Directus to 11.5.0 (#160)

--- packages/e2e/upgrade-directus-version.mjs
+++ packages/e2e/upgrade-directus-version.mjs
@@ -11,8 +11,8 @@ async function writeJSON(path, data) {
   await writeFileSync(path, JSON.stringify(data, null, 2));
 }
 
-const actual = '11.4.1';
-const next = '11.4.1';
+const actual = '11.5.0';
+const next = '11.5.0';
 
 if (actual === next) {
   console.log('Nothing to upgrade');
