9a3d9885824e22c1b656830955a4c648a4bfe996
Edouard Demotes <e.demotes@gmail.com>
2025-04-03 13:14:20 -0400

    chore: upgrade Directus version compatibility

--- docker/build-and-push.mjs
+++ docker/build-and-push.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 
-const latestDirectusVersion = '11.6.0';
+const latestDirectusVersion = '11.6.1';
 const [major, minor, patch] = latestDirectusVersion.split('.');
 const directusVersions = [
   'latest',
--- packages/e2e/upgrade-directus-version.mjs
+++ packages/e2e/upgrade-directus-version.mjs
@@ -11,8 +11,8 @@ async function writeJSON(path, data) {
   await writeFileSync(path, JSON.stringify(data, null, 2));
 }
 
-const actual = '11.6.0';
-const next = '11.6.0';
+const actual = '11.6.1';
+const next = '11.6.1';
 
 if (actual === next) {
   console.log('Nothing to upgrade');
