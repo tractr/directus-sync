a77244e0aebbb3d819bafc2b164697fbb66202db
Edouard Demotes <e.demotes@gmail.com>
2025-09-25 16:43:29 -0400

    fix: update zod version (#188)
    * fix: update zod version
    
    * chore: run format

--- packages/e2e/spec/snapshot/sync-id-map.ts
+++ packages/e2e/spec/snapshot/sync-id-map.ts
@@ -4,9 +4,7 @@ export const syncIdMap = (context: Context) => {
   it('should exclude sync id map from the snapshot', async () => {
     // --------------------------------------------------------------------
     // Init sync client and push
-    const sync = await context.getSync(
-      'sources/snapshot-with-sync-id-map',
-    );
+    const sync = await context.getSync('sources/snapshot-with-sync-id-map');
 
     const diffOutput = await sync.diff();
     expect(diffOutput).toContain(debug('[snapshot] No changes to apply'));
