53eb1adb6b1d1193ccf57d46a4de6c3217d500ed
Edouard Demotes <e.demotes@gmail.com>
2026-02-16 23:34:07 -0500

    chore: directus 11.15.3 compatibility

--- docker/build-and-push.mjs
+++ docker/build-and-push.mjs
@@ -1,6 +1,6 @@
 #!/usr/bin/env zx
 
-const latestDirectusVersion = '11.15.2';
+const latestDirectusVersion = '11.15.3';
 const [major, minor, patch] = latestDirectusVersion.split('.');
 const directusVersions = [
   'latest',
