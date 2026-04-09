965635a151ae03f422d669698ae7ebadf8af2e33
Edouard Demotes <e.demotes@gmail.com>
2026-04-09 10:36:30 -0400

    fix(cli): update PoliciesDataMapper to conditionally set fieldsToIgnore

--- packages/cli/src/lib/services/collections/policies/data-mapper.ts
+++ packages/cli/src/lib/services/collections/policies/data-mapper.ts
@@ -4,18 +4,27 @@ import { LoggerService } from '../../logger';
 import { POLICIES_COLLECTION } from './constants';
 import { DirectusPolicy } from './interfaces';
 import { RolesIdMapperClient } from '../roles';
+import { ConfigService } from '../../config';
 
 @Service()
 export class PoliciesDataMapper extends DataMapper<DirectusPolicy> {
-  protected fieldsToIgnore: Field<DirectusPolicy>[] = ['users', 'permissions'];
-  protected idMappers: IdMappers<DirectusPolicy> = {
-    roles: {
-      // @ts-expect-error TODO: Bad SDK Typing
-      role: Container.get(RolesIdMapperClient),
-    },
-  };
+  protected fieldsToIgnore: Field<DirectusPolicy>[];
+  protected idMappers: IdMappers<DirectusPolicy>;
 
-  constructor(loggerService: LoggerService) {
+  constructor(loggerService: LoggerService, config: ConfigService) {
     super(loggerService.getChild(POLICIES_COLLECTION));
+
+    if (config.shouldSyncPolicyRoles()) {
+      this.fieldsToIgnore = ['users', 'permissions'];
+      this.idMappers = {
+        roles: {
+          // @ts-expect-error TODO: Bad SDK Typing
+          role: Container.get(RolesIdMapperClient),
+        },
+      };
+    } else {
+      this.fieldsToIgnore = ['users', 'permissions', 'roles'];
+      this.idMappers = {};
+    }
   }
 }
